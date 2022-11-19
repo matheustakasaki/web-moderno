e7cd2dba9abf371eeabec0aec81268db298ad47b
matheustakasaki <matheus8antunes@gmail.com>
2022-11-19 16:41:09 -0300

    Finalizando capítulo sobre estruturas de controle

--- exercicios/javascript-estruturas-controle/for1.js
+++ exercicios/javascript-estruturas-controle/for1.js
@@ -2,21 +2,19 @@ let contador = 1;
 
 // Estrutura de controle com while
 while (contador <= 10) {
-    console.log(`contador: ${contador}`);
-    contador++
+	console.log(`WHILE: ${contador}`);
+	contador++
 }
 
 
 // FOR com estrutura: inicializador | condição | incremento
-for (let i = 1; i<= 10; i++) {
-    console.log(`contadorFOR: ${i}`)
-    
+for (let i = 1; i <= 10; i++) {
+	console.log(`FOR: ${i}`);
 }
 
 
 // Utilizando FOR para percorrer array
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7];
 for (let i = 0; i < notas.length; i++) {
-  console.log(`notas: ${notas[i]}`);
-    
+	console.log(`notas: ${notas[i]}`);
 }
