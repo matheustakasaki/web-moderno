6e7fd491fd1390c2518d2d7c700331f5d2d7228a
matheustakasaki <matheus8antunes@gmail.com>
2022-11-27 21:52:12 -0300

    Explicando função para criação de produtos

--- exercicios/javascript-funcao/factory2.js
+++ exercicios/javascript-funcao/factory2.js
@@ -1,4 +1,5 @@
 // Função factory que cria produtos com atributos
+// O atributo 'desconto' vem por padrão com o valor 0.1. Ou seja, se ele não for fornecido na chamada da função, o valor adotado será 0.1
 function criarProduto(nome, preco, desconto = 0.1) {
 	return {
 		nome,
