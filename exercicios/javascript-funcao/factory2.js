6827952dedfd9bd5235bc7937666396a31924160
matheustakasaki <matheus8antunes@gmail.com>
2022-12-01 23:49:19 -0300

    Explicando tecnicamente feature de js

--- exercicios/javascript-funcao/factory2.js
+++ exercicios/javascript-funcao/factory2.js
@@ -1,5 +1,12 @@
 // Função factory que cria produtos com atributos
 // O atributo 'desconto' vem por padrão com o valor 0.1. Ou seja, se ele não for fornecido na chamada da função, o valor adotado será 0.1
+
+
+/*
+// Devido a um recurso do js, não é preciso realizar a notação ponto no retorno da função
+
+
+*/
 function criarProduto(nome, preco, desconto = 0.1) {
 	return {
 		nome,
