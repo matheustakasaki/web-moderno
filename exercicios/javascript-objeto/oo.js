8dd9d27d4458c303b15747cfe690d841a2bfe835
matheustakasaki <matheus8antunes@gmail.com>
2022-12-07 23:46:18 -0300

    Revisando conceitos de OO

---
+++ exercicios/javascript-objeto/oo.js
@@ -0,0 +1,18 @@
+// Código não executável
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+	valor1,
+	valor2,
+	valor3,
+	processamento() {
+		// ...
+	}
+}
+
+// Princípios Importantes
+// 1. Abstração: Traduzir elementos da vida real a um objeto em nível de código. Exemplo: Carro, Funcionário, Cachorro, etc.
+// 2. Encapsulamento: Ter detalhes de implementação escondidos e disponibilizar informações necessárias para o objeto.
