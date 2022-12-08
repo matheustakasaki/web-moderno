17f0b965da999aabbc7c3de2ea2158a2bcc51ebb
matheustakasaki <matheus8antunes@gmail.com>
2022-12-08 00:00:27 -0300

    release:Revisando conceitos de OO

--- exercicios/javascript-objeto/oo.js
+++ exercicios/javascript-objeto/oo.js
@@ -16,3 +16,5 @@ objeto = {
 // Princípios Importantes
 // 1. Abstração: Traduzir elementos da vida real a um objeto em nível de código. Exemplo: Carro, Funcionário, Cachorro, etc.
 // 2. Encapsulamento: Ter detalhes de implementação escondidos e disponibilizar informações necessárias para o objeto.
+// 3. Herança: A tese da herança em O.O, é receber atributos e comportamentos de um objeto pai.
+// 4. Polimorfismo: A partir de uma variável de um tipo mais genérico, somos capazes de atribuir vários tipos concretos - ou subtipos - a ela.
