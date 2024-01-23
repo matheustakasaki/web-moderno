57ff173a6cd07e4025304498eb8dce00a5633f08
matheustakasaki <matheus8antunes@gmail.com>
2024-01-22 23:55:40 -0300

    solucionando desafio da chinesa mais pobre

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,7 +1,18 @@
-const url = `http://files.cod3r.com.br/curso-js/funcionarios.json`
-const axios = require(`axios`)
+const url = `http://files.cod3r.com.br/curso-js/funcionarios.json`;
+const axios = require(`axios`);
 
-axios.get(url).then(response => {
-	const funcionarios = response.data
-	console.log(funcionarios)
-})
+const chinesa = (f) => f.pais === `China`;
+const mulheres = (f) => f.genero === `F`;
+const menorSalario = (pessoa, pessoaAtual) => {
+	return pessoa.salario < pessoaAtual.salario ? pessoa : pessoaAtual;
+};
+
+axios.get(url).then((response) => {
+	const funcionarios = response.data;
+
+	// console.log(funcionarios);
+	// Desafio : devolver a mulher chinesa com menor salario
+
+	const func = funcionarios.filter(chinesa).filter(mulheres).reduce(menorSalario)
+	console.log(func)
+});
