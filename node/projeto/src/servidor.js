08e50eff43a67033d3693dad03218370359eaa3d
matheustakasaki <matheus8antunes@gmail.com>
2024-02-01 00:11:28 -0300

    Projeto API com Express - Implementando Métodos #2

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,7 +2,10 @@ const porta = 3003;
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const bancoDeDados = require("./bandoDeDados");
+
+app.use(bodyParser.urlencoded({ extended: true })); // Como nao tem url especifica, vai aplicar o middleware para todas
 
 app.get("/produtos", (req, res, next) => {
 	res.send(bancoDeDados.getProdutos); // O send() Converte  objeto para json
@@ -14,7 +17,7 @@ app.get("/produtos/:id", (req, res, next) => {
 
 app.post("/produtos", (req, res, next) => {
 	const produto = bancoDeDados.salvarProduto({
-		nome: req.body.name,
+		nome: req.body.nome,
 		preco: req.body.preco,
 	});
 	res.send(produto); // vira JSON
