a52297922c00fe7d740a11fdb9f9c98f771886b0
matheustakasaki <matheus8antunes@gmail.com>
2024-01-31 23:55:39 -0300

    Projeto API com Express - Implementando Post

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,22 @@ const porta = 3003;
 
 const express = require("express");
 const app = express();
+const bancoDeDados = require("./bandoDeDados");
 
 app.get("/produtos", (req, res, next) => {
-	res.send({ nome: `Notebook`, preco: 123.45 }); // O send() Converte  objeto para json
+	res.send(bancoDeDados.getProdutos); // O send() Converte  objeto para json
+});
+
+app.get("/produtos/:id", (req, res, next) => {
+	res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post("/produtos", (req, res, next) => {
+	const produto = bancoDeDados.salvarProduto({
+		nome: req.body.name,
+		preco: req.body.preco,
+	});
+	res.send(produto); // vira JSON
 });
 
 app.listen(porta, () => {
