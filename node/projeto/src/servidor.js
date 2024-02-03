852687f918a09087e37cb0bf8f36f83609a90169
matheustakasaki <matheus8antunes@gmail.com>
2024-02-02 22:59:18 -0300

    PROJETO API Express - corrigindo erros e fechando todos os métodos CRUD

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -3,12 +3,12 @@ const porta = 3003;
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const bancoDeDados = require("./bandoDeDados");
+const bancoDeDados = require("./bancoDeDados");
 
 app.use(bodyParser.urlencoded({ extended: true })); // Como nao tem url especifica, vai aplicar o middleware para todas
 
 app.get("/produtos", (req, res, next) => {
-	res.send(bancoDeDados.getProdutos); // O send() Converte  objeto para json
+	res.send(bancoDeDados.getProdutos()); // O send() Converte  objeto para json
 });
 
 app.get("/produtos/:id", (req, res, next) => {
@@ -23,6 +23,18 @@ app.post("/produtos", (req, res, next) => {
 	res.send(produto); // vira JSON
 });
 
+app.put("/produtos/:id", (req, res, next) => {
+	const produto = bancoDeDados.salvarProduto({
+		id:req.params.id,
+		nome: req.body.nome,
+		preco: req.body.preco,
+	});
+	res.send(produto); // vira JSON
+});
+app.delete("/produtos/:id", (req, res, next) => {
+	const produto = bancoDeDados.excluirProduto(req.params.id)
+	res.send(produto); // vira JSON
+});
 app.listen(porta, () => {
 	console.log("Servidor executando na porta " + porta);
 });
