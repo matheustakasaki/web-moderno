0ce91d2aaf4a617c125c44e6fd7cc05e1518fcbc
matheustakasaki <matheus8antunes@gmail.com>
2024-02-04 11:39:12 -0300

    taggedTemplate esnext

---
+++ esnext/taggedTemplate1.js
@@ -0,0 +1,11 @@
+// tagged template  - processsa o template dentro de uma função.
+function tag(partes, ...valores) {
+	console.log(partes);
+	console.log(valores);
+	return "Outra string";
+}
+
+const aluno = "Gui";
+const situacao = "Aprovado";
+
+console.log(tag `${aluno} está ${situacao}.`);
