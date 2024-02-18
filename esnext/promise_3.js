795b6b88a1b1dc2137e70ced9786cdd60492a0e2
matheustakasaki <matheus8antunes@gmail.com>
2024-02-18 14:01:24 -0300

    exemplo de promise v3

---
+++ esnext/promise_3.js
@@ -0,0 +1,16 @@
+function gerarNumeros(min, max) {
+	if (min > max) {
+		[max, min] = [min, max]; // Destructuring = Definição de array
+	}
+
+	return new Promise((resolve) => {
+		const fator = max - min + 1;
+		const aleatorio = parseInt(Math.random() * fator) + min;
+		resolve(aleatorio);
+	});
+}
+
+gerarNumeros(1, 60)
+	.then((num) => num * 10)
+	.then((numX10) => `O número gerado foi ${numX10}`)
+	.then(console.log);
