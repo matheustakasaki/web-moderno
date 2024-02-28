9ce01f22730d38434aee147f2507660d8f24e497
matheustakasaki <matheus8antunes@gmail.com>
2024-02-27 22:51:26 -0300

    revisando sobre async/await

--- esnext/async_await_1.js
+++ esnext/async_await_1.js
@@ -1,12 +1,31 @@
 function esperarPor(tempo = 2000) {
 	return new Promise(function (resolve) {
-		setTimeout(function () {
-			console.log("Executando promise...");
-			resolve();
-		}, tempo);
+		setTimeout(() => resolve(), tempo);
 	});
 }
 
-await esperarPor(3000)
-.then(esperarPor)
-.then(esperarPor)
+function retornarValor() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(10)
+		}, 5000);
+	});
+}
+// await esperarPor(3000).then(esperarPor).then(esperarPor);
+
+async function executar() {
+
+	let valor = await retornarValor()
+	await esperarPor(2000)
+	console.log(`Async/await ${valor}`)
+
+	await esperarPor(2000)
+	console.log(`Async/await ${valor + 1}`)
+
+	await esperarPor(2000)
+	console.log(`Async/await ${valor + 2}`);
+
+	return valor + 3;
+}
+
+executar().then(console.log)
